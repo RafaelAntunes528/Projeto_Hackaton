ec24c859daf88f6303aab1928504238603d2346e
AntCatela <antonio99catela@gmail.com>
2022-10-19 17:58:01 +0100

    Morador componente feito

--- projeto/pages/index.js
+++ projeto/pages/index.js
@@ -5,11 +5,12 @@ import CartõesPagina from '../src/components/cartõesPagina'
 import Login from '../src/components/login'
 import styles from '../styles/Home.module.css'
 import HomePage from '../src/components/mainPage'
+import Morador from '../src/components/morador'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <HomePage></HomePage>
+    <Morador></Morador>
     </div>
   )
 }
