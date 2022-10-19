92a8e2b3cf2bf2771c6ba37317ed30c64fdfda2b
RafaelAntunes528 <rafa13ant@gmail.com>
2022-10-20 00:21:02 +0100

    MainPage Update

--- projeto/pages/api/index.js
+++ projeto/pages/api/index.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     if (req.method === "GET") {
         console.log("GET")
         const dados = await getAllCondominio(req.body)
-        res.status(200).json(dados)
+        res.status(200).json({body: dados})
         console.log(dados)
         console.log("RES")
 
--- projeto/pages/index.js
+++ projeto/pages/index.js
@@ -9,13 +9,9 @@ import PopUpCriarCon from '../src/components/popUpCriarCon'
 export default function Home() {
   return (
     <div className={styles.container}>
-<<<<<<< HEAD
 
       <HomePage/>
 
-=======
-     <Login></Login>
->>>>>>> d90ddb3c49fb2dfcb00f25459c71c1131ea0fe5f
     </div>
   )
 }
